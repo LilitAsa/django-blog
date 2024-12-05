26e57b955a085b1186474e10effe1c26c3e6bda1
LilitAsa <lilitasa24@yahoo.com>
2024-12-05 11:36:30 +0300

    created include & template tags - single & inclusion

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from django.template.defaultfilters import title
+from unicodedata import category
+
+from blog.templatetags.blog_tags import register
 
 data_from_db = [
 	{
@@ -7,7 +11,9 @@ data_from_db = [
 		"title": "CSTO should not have fought for Yerevan in Karabakh, Putin says",
 		"is_published": True,
 		"content": """
-			The Collective Security Treaty Organization (CSTO) should not have fought for Yerevan in Karabakh; in this case,
+			The <span style = "color:red"> Collective </span> Security 
+			
+			Treaty Organization (CSTO) should not have fought for Yerevan in Karabakh; in this case,
 			there was no external aggression against Armenia, Russian President Vladimir Putin said, TASS reports. Also,
 			the Russian president did not rule out that Armenia will return to full-time work within the CSTO.
 			He added that Armenia has not yet announced its withdrawal from the CSTO and supports all the
@@ -36,6 +42,14 @@ data_from_db = [
 	}
 ]
 
+categories_from_db = [
+	{"id": 1, "name": "Traveling"},
+	{"id": 2, "name": "Sport"},
+	{"id": 3, "name": "IT & Development"},
+	{"id": 4, "name": "Cars"},
+	{"id": 5, "name": "Fashion"},
+]
+
 
 def index(request):
 	data = {
@@ -43,15 +57,18 @@ def index(request):
 		"description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
 		"posts": data_from_db,
 	}
-	return render(request, "blog/index.html", data)
+	return render(request, "blog/home.html", data)
 
 
 def about(request):
 	return render(request, "blog/about.html", {"title": "About Page"})
 
 
-def post_more(request, post_id):
+def show_more(request, post_id):
 	return render(request, "blog/read-more.html", {"title": "Read More"})
+
+def show_category(request,cat_id):
+	return render(request,"blog/category.html",{"title": f"Category: {cat_id}"})
 
 
 def add_post(request):
